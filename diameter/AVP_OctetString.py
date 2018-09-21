9b815b8acdffe6190044e9df262eb1f2b17e49df
Valentin Nechayev <Valentin.Nechayev@portaone.com>
2018-09-21 13:10:52 +0300

    AVP_OctetString: provide the same AVP methods as in others

--- diameter/AVP_OctetString.py
+++ diameter/AVP_OctetString.py
@@ -6,5 +6,24 @@ class AVP_OctetString(AVP):
     def __init__(self,code=0,payload="",vendor_id=0):
         AVP.__init__(self,code,payload,vendor_id)
 
+    def queryValue(self):
+        """Returns the payload as a string (Python2: equal to bytes)"""
+        return str(self.payload)
+
+    def setValue(self,value):
+        self.payload = str(value)
+
+    def __str__(self):
+        return AVP.str_prefix__(self) + " " + self.queryValue()
+
+    @staticmethod
+    def narrow(avp):
+        """Convert generic AVP to AVP_OctetString
+        """
+        a = AVP_OctetString(avp.code, vendor_id=avp.vendor_id)
+        a.flags = avp.flags
+        a.payload = avp.payload
+        return a
+
 def _unittest():
     pass
